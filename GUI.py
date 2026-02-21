f407c379b7395f3e00d914772049aafe5a127784
Mavis Phaphol <114840330+MavisPhaphol@users.noreply.github.com>
2026-02-21 01:15:23 +0000

    Add input fields and buttons for assignment management in GUI

--- GUI.py
+++ GUI.py
@@ -17,15 +17,15 @@ listbox.pack() #this will display the list box on the window
 # and https://stackoverflow.com/questions/43148463/how-to-place-widgets-next-to-each-other/43149015#43149015
 assignmentLabel = Label(window, text="Assignment Title:", fg="white", background="#343940")
 assignmentLabel.pack()
-assignmentInput = Text(window, height = 10, width = 50) #text box for users to put assignment infomation
-assignmentInput.pack()
+assignmentInput = Text(window, height = 5, width = 20) #text box for users to put assignment infomation
+assignmentInput.pack() 
 dueDateLabel = Label(window, text="Due Date:", fg="white", background="#343940")
 dueDateLabel.pack()
-dueDateInput = Text(window, height = 10, width = 20)
+dueDateInput = Text(window, height = 5, width = 20)
 dueDateInput.pack()
 difficultyLabel = Label(window, text="Difficulty:", fg="white", background="#343940")
 difficultyLabel.pack()
-difficultyInput = Text(window, height = 10, width = 20)
+difficultyInput = Text(window, height = 5, width = 20)
 difficultyInput.pack()
 
 smartStudyPlanner=study_planner()
@@ -42,5 +42,13 @@ def generate_planner():
     listbox.delete(0, END) #this will clear the list box before displaying the sorted assignments
     for assignment in smartStudyPlanner.assignments:
         listbox.insert(END, f"{assignment["name"]} - Due: {assignment.due_date} - Difficulty: {assignment.difficulty} - Priority: {assignment.priority}") #this will display the sorted assignment information in the list box
+
+#this will be the button to add the assignment information to the study planner
+button=Button(window, text="Add Assignment", command=add_assignment_gui)
+button.pack()
+
+#this will be the button to generate the sorted assignment list in the list box
+secondButton=Button(window, text="Generate Study Planner", command=generate_planner)
+secondButton.pack()
 
 window.mainloop() #this will display window size once you hit run
